64ab9191ec6d6021d58f419ebd2f03ea3dbb1a81
evranahmet <ahmetevran.ae@gmail.com>
2024-06-13 01:25:31 +0300

    13/06

---
+++ src/test/java/clarusway/homework/Hw30.java
@@ -0,0 +1,24 @@
+package clarusway.homework;
+
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Hw30 {
+    /*
+    Given
+        Go to https://www.hepsiburada.com/bilgisayarlar-c-2147483646
+    When
+        Click all marks and print their names
+        (Soldaki markaların hepsine tıklayıp adlarını yazdırınız)
+    */
+
+    @Test
+    public void Hw30() throws InterruptedException {
+
+
+    }
+}
